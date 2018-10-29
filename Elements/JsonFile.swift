03ad4baca6f48c5532f1b03c455960c0ff324e6b
Paolo Rocca <hispanico94@gmail.com>
2018-10-29 11:51:37 +0100

    Removed JsonReader, now initialization is done in JsonFile

--- Elements/JsonFile.swift
+++ Elements/JsonFile.swift
@@ -3,26 +3,14 @@ import Foundation
 struct JsonFile: Codable {
     let version: Int
     let passes: [Pass]
-}
-
-struct JsonReader {
-    static func readJson() -> JsonFile {
-        let wrappedUrl = Bundle.main.url(forResource: "Passes", withExtension: "json")
-        
-        guard let url = wrappedUrl else {
-            fatalError("Unable to get the URL of Passes.json")
-        }
-        
-        return getJsonFileFromJsonOrCrash(jsonUrl: url)
-    }
     
-    private static func getJsonFileFromJsonOrCrash(jsonUrl url: URL) -> JsonFile {
+    init?(fromJsonWithUrl url: URL) {
         do {
             let jsonData = try Data(contentsOf: url)
-            let jsonFile = try JSONDecoder().decode(JsonFile.self, from: jsonData)
-            return jsonFile
+            self = try JSONDecoder().decode(JsonFile.self, from: jsonData)
         } catch {
-            fatalError("Error during reading of Passes.json: \(error.localizedDescription)")
+            print("Error during reading of Passes.json: \(error.localizedDescription)")
+            return nil
         }
     }
 }
