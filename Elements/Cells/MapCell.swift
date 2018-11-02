b27d01e92fbd3132a3b7190191a6260adec80deb
Paolo Rocca <hispanico94@gmail.com>
2018-11-02 18:52:21 +0100

    Initial cell setup

--- Elements/Cells/MapCell.swift
+++ Elements/Cells/MapCell.swift
@@ -1,21 +1,38 @@
 import UIKit
+import MapKit
 
 class MapCell: UITableViewCell {
     static let defaultIdentifier = "MapCellIdentifier"
+    
+    @IBOutlet weak var mapView: MKMapView! {
+        didSet {
+            mapView.isScrollEnabled = false
+            //mapView.isZoomEnabled = false
+            mapView.isPitchEnabled = false
+            mapView.isRotateEnabled = false
+            mapView.showsUserLocation = true
+        }
+    }
+    
+    private let regionRadius: CLLocationDistance = 10000
+    
+    private var coordinates: CLLocationCoordinate2D! {
+        didSet {
+            setMap()
+        }
+    }
     
     static func getCell() -> MapCell {
         return UINib(nibName: "MapCell", bundle: nil).instantiate(withOwner: nil, options: nil).first as! MapCell
     }
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    func set(with coordinates: CLLocationCoordinate2D) -> MapCell {
+        self.coordinates = coordinates
+        return self
     }
     
+    private func setMap() {
+        let region = MKCoordinateRegion(center: coordinates, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
+        mapView.setRegion(region, animated: true)
+    }
 }
