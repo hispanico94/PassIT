f782d89995fe940c378d3d35e683790ccf7ada36
Paolo Rocca <hispanico94@gmail.com>
2018-11-04 17:24:49 +0100

    Added map zoom without scroll

--- Elements/Cells/MapCell.swift
+++ Elements/Cells/MapCell.swift
@@ -7,14 +7,17 @@ class MapCell: UITableViewCell {
     @IBOutlet weak var mapView: MKMapView! {
         didSet {
             mapView.isScrollEnabled = false
-            //mapView.isZoomEnabled = false
+            mapView.isZoomEnabled = false
             mapView.isPitchEnabled = false
             mapView.isRotateEnabled = false
             mapView.showsUserLocation = true
+            let pinchRecognizer = UIPinchGestureRecognizer(target: self, action: #selector(userDidPinch(_:)))
+            mapView.addGestureRecognizer(pinchRecognizer)
         }
     }
     
-    private let regionRadius: CLLocationDistance = 10000
+    private let initialRegionRadius: CLLocationDistance = 10000
+    private var regionDisplayed: MKCoordinateRegion!
     
     private var coordinates: CLLocationCoordinate2D! {
         didSet {
@@ -27,12 +30,29 @@ class MapCell: UITableViewCell {
     }
     
     func set(with coordinates: CLLocationCoordinate2D) -> MapCell {
+        self.selectionStyle = .none
         self.coordinates = coordinates
         return self
     }
     
     private func setMap() {
-        let region = MKCoordinateRegion(center: coordinates, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
-        mapView.setRegion(region, animated: true)
+        regionDisplayed = MKCoordinateRegion(center: coordinates, latitudinalMeters: initialRegionRadius, longitudinalMeters: initialRegionRadius)
+        mapView.setRegion(regionDisplayed, animated: true)
+    }
+    
+    @objc func userDidPinch(_ sender: UIGestureRecognizer) {
+        guard let sender = sender as? UIPinchGestureRecognizer else { return }
+        
+        let latitudeDelta = regionDisplayed.span.latitudeDelta / CLLocationDegrees(sender.scale)
+        let longitudeDelta = regionDisplayed.span.longitudeDelta / CLLocationDegrees(sender.scale)
+        
+        let newSpan = MKCoordinateSpan(latitudeDelta: latitudeDelta, longitudeDelta: longitudeDelta)
+        let newRegion = MKCoordinateRegion(center: coordinates, span: newSpan)
+        
+        mapView.setRegion(newRegion, animated: false)
+        
+        if sender.state == .ended {
+            regionDisplayed = self.mapView.region
+        }
     }
 }
